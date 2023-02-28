e7092cd3eb6622553c2c25f15c6f8a3f59219e44
unlimited <osx@pop-os.localdomain>
2023-02-28 02:52:35 -0800

    finish week1

--- code/w1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ code/w1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,18 @@
-package com.example.buttonapp
+package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById
-
+        findViewById<ImageView>(R.id.imageView2).setOnClickListener(){
+            // print log statement
+            Log.i("Caren", "Clicked on button")
+        }
     }
 }
--- code/w1/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ code/w1/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.buttonapp
+package com.example.myapplication
 
 import org.junit.Test
 
